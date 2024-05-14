1f74a4792688cbff329f54789ce2b4fd05f44bbd
hongsy521 <seoyeong0521@naver.com>
2024-05-14 23:01:14 +0900

    update, delete 구현 완료

--- src/main/java/com/sparta/memo_spring/controller/MemoController.java
+++ src/main/java/com/sparta/memo_spring/controller/MemoController.java
@@ -48,13 +48,31 @@ public class MemoController {
 
     // 메모 변경
     @PutMapping("/memos/{id}")
-    public Long editMemo(@PathVariable int id){
-        return null;
+    public Long editMemo(@PathVariable Long id,@RequestBody MemoRequestDto requestDto){
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            // 메모 가져오기
+            Memo memo = memoList.get(id);
+            // 메모 수정
+            memo.update(requestDto);
+            return memo.getId();
+
+        }else {
+            throw new IllegalArgumentException("Memo not found");
+        }
     }
 
     // 메모 삭제
     @DeleteMapping("/memos/{id}")
-    public Long deleteMemo(@PathVariable int id){
-        return null;
+    public Long deleteMemo(@PathVariable Long id){
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            // 메모 삭제
+            memoList.remove(id);
+            return id;
+
+        }else {
+            throw new IllegalArgumentException("Memo not found");
+        }
     }
 }
