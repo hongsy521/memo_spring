b1a290fb87db3dc7118c2d2b0a4b8f49fc127b49
hongsy521 <seoyeong0521@naver.com>
2024-05-14 22:04:36 +0900

    initial commit

---
+++ src/main/java/com/sparta/memo_spring/MemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.memo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/memo_spring/MemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.memo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
