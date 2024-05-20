79ee7f88feb54344f5c188b2a7b38d0e1707734f
hongsy521 <seoyeong0521@naver.com>
2024-05-20 14:34:42 +0900

    Query method

--- src/main/java/com/sparta/memo_spring/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo_spring/repository/MemoRepository.java
@@ -4,7 +4,9 @@ import com.sparta.memo_spring.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemoRepository extends JpaRepository<Memo,Long> {  // memoRepository 이름으로 bean 등록
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
